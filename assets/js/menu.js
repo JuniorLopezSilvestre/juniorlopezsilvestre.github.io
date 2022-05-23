e2d47ec4cb2b8b9d5cdd6740d2861d9726d584e1
JuniorLopezSilvestre <junior.lopez.silvestre@gmail.com>
2022-05-22 23:40:19 -0500

    Portafolio actualizadp

---
+++ assets/js/menu.js
@@ -0,0 +1,19 @@
+	/*============================================
+	Navigation Functions
+	==============================================*/
+	if ($(window).scrollTop()< ($(window).height()-50)){
+		$('#main-nav').removeClass('scrolled');
+	}
+	else{
+		$('#main-nav').addClass('scrolled');    
+	}
+
+	$(window).scroll(function(){
+		if ($(window).scrollTop()< ($(window).height()-50)){
+			$('#main-nav').removeClass('scrolled');
+		}
+		else{
+			$('#main-nav').addClass('scrolled');    
+		}
+	});
+	
